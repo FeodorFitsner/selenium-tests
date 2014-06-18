e04a7aa1d22924d725756761e1f0bbda51bcd1f6
Feodor Fitsner <feodor@fitsner.com>
2014-06-17 17:04:21 -0700

    FF logging

--- SeleniumTesting/FireFoxTests.cs
+++ SeleniumTesting/FireFoxTests.cs
@@ -22,7 +22,7 @@ namespace SeleniumTesting
             driver = data.GetDriver();
         }
 
-        //[Fact]
+        [Fact]
         public void Google_com_should_return_search_results()
         {
             driver.Navigate().GoToUrl("http://www.google.com/ncr");
@@ -44,6 +44,8 @@ namespace SeleniumTesting
 
         public FireFoxFixture()
         {
+            Environment.SetEnvironmentVariable("webdriver.log.file", "log-file.txt");
+            Environment.SetEnvironmentVariable("webdriver.firefox.logfile", "ff-log.txt");
             driver = new FirefoxDriver();
         }
 
